f83563465be44441fefd809881e731ff49dc022e
lu <henrique3.terceiro@gmail.com>
2024-10-16 22:33:06 -0300

    pages

---
+++ src/app/challenge-sprints/page.tsx
@@ -0,0 +1,11 @@
+const ChallengeSprints = () => {
+    return (
+      <div>
+        <h1>Checkpoints</h1>
+        <p>Listagem de todas as avaliações de ChallengeSprints aqui.</p>
+      </div>
+    )
+  }
+  
+  export default ChallengeSprints;
+  
---
+++ src/app/checkpoint/page.tsx
@@ -0,0 +1,11 @@
+const CheckpointsPage = () => {
+    return (
+      <div>
+        <h1>Checkpoints</h1>
+        <p>Listagem de todas as avaliações de checkpoints aqui.</p>
+      </div>
+    )
+  }
+  
+  export default CheckpointsPage;
+  
