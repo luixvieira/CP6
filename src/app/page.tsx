3cfa3e0b77e0149eee066246b004e14e90d7f9f3
difurigo <furigo9diego@gmail.com>
2024-10-25 23:36:48 -0300

    Feito um pouco da pag de notas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <div></div>
+        <div>
+            <Image 
+                src="/img/bulbassauro.png" 
+                alt="Página de erro" 
+                width={50} 
+                height={50} 
+                placeholder="blur" 
+                blurDataURL="/blur.jpg" 
+                layout="responsive" 
+             />
+        </div>
     );
 }
