b54838c278e472f40a19655b21d099809c281124
luis <henrique3.terceiro@gmail.com>
2024-10-27 20:16:27 -0300

    arumando para o deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import "@/app/globals.css"
-import Cabecalho from "@/components/Cabecalho/Cabecalho";
-import Rodape from "@/components/Rodape/Rodape";
+import "../app/globals.css"
+import Cabecalho from "../components/cabecalho/cabecalho";
+import Rodape from "../components/Rodape/rodape";
 
 export const metadata: Metadata = {
     title: "CP6",
