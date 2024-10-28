7c0ea29a2ac3354f5903c30eece3aa9b95626d6d
luis <henrique3.terceiro@gmail.com>
2024-10-27 21:14:08 -0300

    tipagem gs

--- src/app/global-solution/page.tsx
+++ src/app/global-solution/page.tsx
@@ -3,38 +3,44 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+type Aluno = {
+  nome: string;
+  rm: number;
+};
+
+type Nota = {
+  rm: number;
+  nota: number;
+};
+
 export default function GSPage() {
-  
-  const alunos = [
+  const alunos: Aluno[] = [
     { nome: "Diego Furigo", rm: 558755 },
     { nome: "Luis Henrique", rm: 558935 },
     { nome: "Melissa Pereira", rm: 555656 },
   ];
 
+  const [notas, setNotas] = useState<Nota[]>([]);
 
-
-  const [notas, setNotas] = useState([]);
-
- 
   useEffect(() => {
     // Chamar a API para carregar os dados do JSON
-    fetch('/api/bases/base-global-solution')
+    fetch("/api/bases/base-global-solution")
       .then((response) => {
         if (!response.ok) throw new Error("Erro ao carregar o JSON da API");
         return response.json();
       })
-      .then((data) => {
+      .then((data: Nota[]) => {
         console.log("Dados carregados da API:", data);
         setNotas(data);
       })
       .catch((error) => console.error("Erro ao carregar as notas:", error));
   }, []);
 
-  const calcularMedia = (rm) => {
+  const calcularMedia = (rm: number): string => {
     const notasDoAluno = notas.filter((nota) => nota.rm === rm);
     console.log(`Notas para RM ${rm}:`, notasDoAluno);
-    
-    if (notasDoAluno.length === 0) return 0;
+
+    if (notasDoAluno.length === 0) return "0.00";
 
     const soma = notasDoAluno.reduce((acc, curr) => acc + curr.nota, 0);
     return (soma / notasDoAluno.length).toFixed(2);
@@ -54,12 +60,12 @@ export default function GSPage() {
               <span className="nome">{aluno.nome}</span>
               <span>RM: {aluno.rm}</span>
               <div className="caixa-medias">
-              <span className="medias">Médias {calcularMedia(aluno.rm)} </span>
+                <span className="medias">Médias {calcularMedia(aluno.rm)} </span>
               </div>
             </section>
           </Link>
         ))}
       </div>
     </>
-  )
+  );
 }
